f9f7502010e6a1307a319d99ae7063febc9fd9d1
Teodoro del Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2022-03-03 22:49:52 -0600

    Service Worker: Push

--- 03-service-worker/js/app.js
+++ 03-service-worker/js/app.js
@@ -5,11 +5,17 @@ if ( navigator.serviceWorker ) {
     navigator.serviceWorker.register('/sw.js')
         .then( reg => {
 
-            setTimeout(() => {
-                reg.sync.register('posteo-gatitos');
-                console.log('Se enviaron fotos de gatitos al server...')
-            }, 3000);
+            // setTimeout(() => {
+            //     reg.sync.register('posteo-gatitos');
+            //     console.log('Se enviaron fotos de gatitos al server...')
+            // }, 3000);
 
+            Notification.requestPermission().then( result  => {
+                
+                console.log( result );
+                reg.showNotification('Hola Mundo!');
+                
+            });
         })
 }
 
