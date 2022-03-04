57c909d96c519bae9826b5a4489c781e1102af19
Teodoro del Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2022-03-03 21:40:51 -0600

    Service Worker: Activate

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -7,4 +7,15 @@ self.addEventListener('install', event => {
     // Creamos cache
     
     console.log( 'SW: Instalando SW!! ' )
+
+    // Con esto podemos activarlo al momento de instalar
+    // Es recomendable dejarlo sin el sino es necesario
+    // self.skipWaiting();
+})
+
+// Cuando el SW toma el control de la aplicación
+
+self.addEventListener('activate', event => {
+    // Borrar cache viejo
+    console.log('SW2: Activo y listo para controlar la app ')
 })
