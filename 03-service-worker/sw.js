115aa2a79c79b5c50a43e7cec70f879a50f9b82f
Teodoro del Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2022-03-03 21:53:22 -0600

    event.waitUntil();

--- 03-service-worker/sw.js
+++ 03-service-worker/sw.js
@@ -8,9 +8,21 @@ self.addEventListener('install', event => {
     
     console.log( 'SW: Instalando SW!! ' )
 
+    const instalacion = new Promise( (resolve, reject) => {
+
+        setTimeout(() => {
+            console.log('SW: Instalaciones terminadas');
+            self.skipWaiting();
+            resolve();
+        }, 1000);
+
+    })
     // Con esto podemos activarlo al momento de instalar
     // Es recomendable dejarlo sin el sino es necesario
-    // self.skipWaiting();
+    // 
+
+
+  event.waitUntil( instalacion );  
 })
 
 // Cuando el SW toma el control de la aplicación
