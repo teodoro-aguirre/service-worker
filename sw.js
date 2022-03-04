d442426985330ab62173bf2b7e39658c762601ac
teodoro-aguirre <rodriguezaguirreangelt@gmail.com>
2022-03-03 20:27:22 -0600

    Interceptar y modificar peticiones

--- sw.js
+++ sw.js
@@ -1,13 +1,25 @@
 self.addEventListener('fetch', event => { 
 
-    if(event.request.url.includes('.jpg')){
-        console.log(event.request.url)
+    if( event.request.url.includes('main.jpg') ){
 
-        // let fotoReq = fetch('img/main.jpg')
-        // let fotoReq = fetch(event.request.url)
-        let fotoReq = fetch(event.request)
-
+        let fotoReq = fetch('img/main-patas-arriba.jpg')
         event.respondWith( fotoReq )
+        
     }
+
+    // if( event.request.url.includes('style.css')){
+    //     let respuesta = new Response(`
+    //         body {
+    //             background-color: red !important;
+    //             color: pink;
+    //         }
+    //     `, {
+    //         headers: {
+    //             'Content-Type': 'text/css'
+    //         }
+    //     });
+
+    //     event.respondWith( respuesta )
+    // }
 
 })
