2473aee46896ced96da8215dca873cf4310931b8
teodoro-aguirre <rodriguezaguirreangelt@gmail.com>
2022-03-07 15:23:18 -0600

    Limitar cache dinamico

--- 04-cache-offline-inicio/sw.js
+++ 04-cache-offline-inicio/sw.js
@@ -5,10 +5,25 @@
 
 // const CACHE_NAME = 'cache-1'
 
-const CACHE_STATIC_NAME = 'static-v1'
+const CACHE_STATIC_NAME = 'static-v2'
 const CACHE_DYNAMIC_NAME = 'dynamic-v1'
 
 const CACHE_INMUTABLE_NAME = 'inmutable-v1'
+
+function limpiarCache( cacheName, numeroItems ) {
+
+    caches.open( cacheName )
+        .then( cache => {
+            cache.keys()
+                .then( keys => {
+                    if ( keys.length > numeroItems) {
+                        cache.delete( keys[0] )
+                            .then( limpiarCache(cacheName, numeroItems) )
+                    }
+                })
+        });
+
+}
 
 self.addEventListener('install', e => {
 
@@ -60,6 +75,7 @@ self.addEventListener('fetch', e => {
                     caches.open( CACHE_DYNAMIC_NAME )
                         .then(cache => {
                             cache.put( e.request, newResp );
+                            limpiarCache( CACHE_DYNAMIC_NAME, 3)
                         })
 
                     return newResp.clone()
