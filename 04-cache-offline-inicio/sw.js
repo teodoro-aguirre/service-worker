e36f6910ba48df6bae8ad8510c405524ca585b89
Teodoro del Angel Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2022-03-06 18:02:48 -0600

    Repuesta offline basica

--- 04-cache-offline-inicio/sw.js
+++ 04-cache-offline-inicio/sw.js
@@ -1,3 +1,15 @@
 
 
 
+self.addEventListener('fetch', event => {
+
+    const offlineResp = new Response(`
+        Bienvenido a mi Página web
+        Para usarla necesitas internet
+    `)
+
+    const resp = fetch(event.request)
+                    .catch( () => offlineResp)
+
+    event.respondWith( resp )
+})
