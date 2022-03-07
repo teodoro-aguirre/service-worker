ed1fc7a600e9615a682359eae824890a880e9608
teodoro-aguirre <rodriguezaguirreangelt@gmail.com>
2022-03-07 14:19:56 -0600

    Estrategia: Cache with network fallback

--- 04-cache-offline-inicio/sw.js
+++ 04-cache-offline-inicio/sw.js
@@ -2,10 +2,13 @@
 // El APP SHELL es todo lo que la app requiere
 // para trabajar
 
+
+const CACHE_NAME = 'cache-1'
+
 self.addEventListener('install', e => {
 
     // Guardar APP SHELL a la hora de instalar el SW
-    const cacheProm = caches.open('cache-1')
+    const cacheProm = caches.open(CACHE_NAME)
         .then( cache => {
 
             return cache.addAll([
@@ -28,7 +31,32 @@ self.addEventListener('fetch', e => {
     // 1 Cache only
     // Se ocupa cuando toda la aplicacion solo funciona
     // con el cache
-    e.respondWith( caches.match( e.request ) ) // Lee las respuestas que coinciden con el cache(Todos)
+    // e.respondWith( caches.match( e.request ) ) // Lee las respuestas que coinciden con el cache(Todos)
+
+    
+    // 2- Cache with Network Fallback
+    // Primero necesitamos verificar si el archivo existe en el cache
+    const respuestaCache = caches.match( e.request )
+        .then( res => {
+            if( res ) return res;
+
+            // No existe
+            // tengo que ir a la web
+            console.log('No existe', e.request.url )
+
+            return fetch( e.request ).then( newResp => {
+
+                    caches.open(CACHE_NAME)
+                        .then(cache => {
+                            cache.put( e.request, newResp );
+                        })
+
+                    return newResp.clone()
+
+                })
+    })
+
+    e.respondWith( respuestaCache )
     
 
 
