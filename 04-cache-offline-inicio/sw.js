9b210068ee52595271263e4b4cc7a0e340728040
teodoro-aguirre <rodriguezaguirreangelt@gmail.com>
2022-03-07 14:53:28 -0600

    Cache dinámico - Optimizaciones

--- 04-cache-offline-inicio/sw.js
+++ 04-cache-offline-inicio/sw.js
@@ -3,12 +3,17 @@
 // para trabajar
 
 
-const CACHE_NAME = 'cache-1'
+// const CACHE_NAME = 'cache-1'
+
+const CACHE_STATIC_NAME = 'static-v1'
+const CACHE_DYNAMIC_NAME = 'dynamic-v1'
+
+const CACHE_INMUTABLE_NAME = 'inmutable-v1'
 
 self.addEventListener('install', e => {
 
     // Guardar APP SHELL a la hora de instalar el SW
-    const cacheProm = caches.open(CACHE_NAME)
+    const cacheProm = caches.open(CACHE_STATIC_NAME)
         .then( cache => {
 
             return cache.addAll([
@@ -16,14 +21,20 @@ self.addEventListener('install', e => {
                 '/index.html',
                 '/css/style.css',
                 '/img/main.jpg',
-                'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
                 '/js/app.js'
             ]);
 
         }) 
 
+    const cacheInmutable = caches.open(CACHE_INMUTABLE_NAME)
+        .then( cache => {
+            return cache.addAll([
+                'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+            ])
+        })
+
     // Espera a que una promesa se resuelva
-    e.waitUntil( cacheProm );
+    e.waitUntil( Promise.all([cacheProm, cacheInmutable]) );
 })
 
 // Las estrategias de cache se hacen en el evento fetch
@@ -33,7 +44,7 @@ self.addEventListener('fetch', e => {
     // con el cache
     // e.respondWith( caches.match( e.request ) ) // Lee las respuestas que coinciden con el cache(Todos)
 
-    
+
     // 2- Cache with Network Fallback
     // Primero necesitamos verificar si el archivo existe en el cache
     const respuestaCache = caches.match( e.request )
@@ -46,7 +57,7 @@ self.addEventListener('fetch', e => {
 
             return fetch( e.request ).then( newResp => {
 
-                    caches.open(CACHE_NAME)
+                    caches.open( CACHE_DYNAMIC_NAME )
                         .then(cache => {
                             cache.put( e.request, newResp );
                         })
